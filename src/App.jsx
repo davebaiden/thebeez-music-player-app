31f89ea63502b93f1d482f39883d471843d54d20
David Amissah <david.amissah@margins-id.com>
2025-10-28 12:59:34 +0000

    Add search bar code and homepage code to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import HomePage from "./pages/HomePage";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-blue-600">The Beez Music Player 🎶🐝</h1>
-      <p>Welcome to your music experience &#x1F3B6;</p>
-      <p>A World full of buzzing beez &#x1F41D;</p>
+    <div className="min-h-screen bg-gray-900 text-white">
+      <HomePage />
     </div>
   );
 }
